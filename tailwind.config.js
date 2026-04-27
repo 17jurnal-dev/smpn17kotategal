155492b82764e7883311acd9919aa99ca40100cd
17jurnal-dev <17jurnal@gmail.com>
2026-04-27 06:42:26 +0000

    Fix dependencies and build issues for Vercel deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
+    "./styles/**/*.{css}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 };
